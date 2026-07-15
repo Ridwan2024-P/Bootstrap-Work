547a97282ba9d78755bda6809262c0b6e7f1215e
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-15 15:43:58 +0600

    done indexpage and doctor page

---
+++ Exercise/Doctor House/script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
