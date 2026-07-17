3829dc5263e68ec9311439a96782c3243064fe19
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-17 11:32:01 +0600

    fix-the doctor-appointment-page

--- Exercise/Doctor House/script.js
+++ Exercise/Doctor House/script.js
@@ -7,3 +7,42 @@ window.addEventListener("scroll", function () {
         navbar.classList.remove("scrolled");
     }
 });
+
+
+// Appointment
+ document.addEventListener("DOMContentLoaded", function () {
+
+    setTimeout(() => {
+
+        const calendar = document.getElementById("myCalendar");
+
+        calendar.addEventListener("click", function (e) {
+
+            const day = e.target.closest(".calendar-cell,.calendar-day");
+
+            if (!day) return;
+
+            console.log(day); 
+
+        
+            let date =
+                day.dataset.date ||
+                day.getAttribute("data-date") ||
+                day.getAttribute("aria-label");
+
+            if (!date) {
+                const dayNumber = day.innerText.trim();
+                date = `July ${dayNumber}, 2026`;
+            }
+
+            document.getElementById("selectedDate").value = date;
+
+            new bootstrap.Modal(
+                document.getElementById("appointmentModal")
+            ).show();
+
+        });
+
+    }, 500);
+
+});
